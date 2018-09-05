5e22c6431e96808b3fd3560332642733f9170d85
Shannon Holmes <snholmes13@gmail.com>
2018-09-05 13:04:09 +0100

    populate seeds file

---
+++ server/db/seeds.js
@@ -0,0 +1,20 @@
+use bucketlist;
+db.dropDatabase();
+
+db.items.insertMany([
+  {
+    name: "Travel to Fiji",
+    details: "Do by 60",
+    completed: "false"
+  },
+  {
+    name: "Meet the Queen",
+    details: "On 10 November 2018 at the Royal British Legion Festival of Remembrance at the Royal Albert Hall, London",
+    completed: "false"
+  },
+  {
+    name: "Go White Water Rafting",
+    details: "Splash White Water Rafting with pals",
+    completed: "True"
+  }
+])
