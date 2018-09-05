d9288f86beb35e766cfa29babb8b12efede5a9ed
fddata <fraserjdouglas@gmail.com>
2018-09-05 13:43:48 +0100

    fixes typos to server.js

--- server/server.js
+++ server/server.js
@@ -3,19 +3,19 @@ const app = express();
 const path = require('path');
 const parser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('.helpers/create_router.js');
+const createRouter = require('./helpers/create_router.js');
 
-const pubblicPath = path.join(__dirname, '../client/public');
+const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
 
 app.use(parser.json());
 
-MongoClient.connect('mongodb://localhost27017')
+MongoClient.connect('mongodb://localhost:27017')
   .then((client) => {
     const db = client.db('bucketlist');
     const itemsCollection = db.collection('items');
     const itemsRouter = createRouter(itemsCollection);
-    app.use('api/bucketlist', itemsRouter);
+    app.use('/api/bucketlist', itemsRouter);
   })
   .catch(console.err);
 
