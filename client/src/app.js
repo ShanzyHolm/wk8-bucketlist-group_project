c957e844823e84f78475cd2b2018e15981233a12
Shannon Holmes <snholmes13@gmail.com>
2018-09-05 15:12:48 +0100

    add app confirm POST

---
+++ client/src/app.js
@@ -0,0 +1,16 @@
+const FormView = require('./views/form_view.js');
+// const GridView = require('./views/grid_view.js');
+// const ItemView = require('./views/item_view.js');
+const BucketList = require('./models/bucketlist.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const APIURL = "http://localhost:3000/api/bucketlist";
+
+  const bucketList = new BucketList(APIURL);
+  bucketList.bindEvents()
+
+  const itemForm = document.querySelector('#bucketlist-form');
+  // console.log(itemForm);
+  const itemFormView = new FormView(itemForm);
+  itemFormView.bindEvents();
+});
--- server/db/seeds.js
+++ server/db/seeds.js
@@ -5,16 +5,16 @@ db.items.insertMany([
   {
     name: "Travel to Fiji",
     details: "Do by 60",
-    completed: "false"
+    completed: false
   },
   {
     name: "Meet the Queen",
     details: "On 10 November 2018 at the Royal British Legion Festival of Remembrance at the Royal Albert Hall, London",
-    completed: "false"
+    completed: false
   },
   {
     name: "Go White Water Rafting",
     details: "Splash White Water Rafting with pals",
-    completed: "True"
+    completed: true
   }
 ])
